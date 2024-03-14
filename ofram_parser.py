c69920b1ddd5eeafad97ea04d77b78d1997341ad
Caruda55 <122803959+Caruda55@users.noreply.github.com>
2024-03-14 18:40:02 +0300

    Update ofram_parser.py

--- ofram_parser.py
+++ ofram_parser.py
@@ -82,7 +82,7 @@ def csv_writer(all_product):
 
 # Заголовки и список категорий/подкатегорий
 headers = {'accept': '*/*',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+           'User-Agent': 'нужно вставить'
            }
 
 categories = [
